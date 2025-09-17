5357af3de0457f690a1d263f3141bd37a68acc56
Linaleri <>
2025-09-18 01:53:12 +0200

    JUnit/NGT test addedn(:

---
+++ test/yezzimetsuccess/LoginNGTest.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package yezzimetsuccess;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Bongi
+ */
+public class LoginNGTest {
+    
+    public LoginNGTest() {
+    }
+
+    /**
+     * Test of checkUsername method, of class Login.
+     */
+    @Test
+    public void testCheckUsername() {
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    /**
+     * Test of checkCellPhoneNumber method, of class Login.
+     */
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+    }
+    
+}
