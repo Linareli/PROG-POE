f6c066ba024fd6e1b3b0c8cbbc5285b8721bab0d
Linaleri <>
2025-10-13 22:58:54 +0200

    unit tests created

---
+++ test/yezzimetsuccess/MessageNGTest.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package yezzimetsuccess;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Bongi
+ */
+public class MessageNGTest {
+    
+    public MessageNGTest() {
+    }
+
+    /**
+     * Test of checkrecipientCell method, of class Message.
+     */
+    @Test
+    public void testCheckrecipientCell() {
+        
+    }
+
+    /**
+     * Test of checkMessageID method, of class Message.
+     */
+    @Test
+    public void testCheckMessageID() {
+    }
+
+    /**
+     * Test of createMessageHash method, of class Message.
+     */
+    @Test
+    public void testCreateMessageHash() {
+    }
+
+    /**
+     * Test of sentMessage method, of class Message.
+     */
+    @Test
+    public void testSentMessage() {
+    }
+
+    /**
+     * Test of printMessage method, of class Message.
+     */
+    @Test
+    public void testPrintMessage() {
+    }
+
+    /**
+     * Test of returnTotalMessages method, of class Message.
+     */
+    @Test
+    public void testReturnTotalMessages() {
+    }
+
+    /**
+     * Test of storeMessage method, of class Message.
+     */
+    @Test
+    public void testStoreMessage() {
+    }
+    
+}
